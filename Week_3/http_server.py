eaa984401670732ef8531c325a4859377e0e4adc
codersachin26 <sachinindoriya63@gmail.com>
2022-04-19 16:08:47 +0530

    added CRUD operations to db

--- Week_3/http_server.py
+++ Week_3/http_server.py
@@ -1,8 +1,12 @@
 import csv
 import io
-from flask import Flask, request, make_response, jsonify
+import logging
 
+from flask import Flask, request, make_response, jsonify, render_template
+
+from Week_3.helpers import csv_to_list
 from Week_3.sql_db import SqlDB
+import mysql.connector as mySql_connector
 
 # mysql connection credentials
 USERNAME = 'sachin'
@@ -13,30 +17,154 @@ HOST = 'localhost'
 app = Flask(__name__)
 
 
-@app.route('/upload_csv', methods=['POST'])
-def upload_file():
-    if request.method == 'POST':
-        f = request.files['file']
-
-        with io.TextIOWrapper(f, encoding='utf-8') as text_file:
-            reader = csv.DictReader(text_file, delimiter=',')
-            data = [list(row.values()) for row in reader]
+@app.route('/insert_into_db', methods=['POST', 'GET'])
+def insert_to_db():
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        csv_file = request.files['file']
 
         db_conn = SqlDB(USERNAME, PASSWORD, HOST, DATABASE)
 
-        if db_conn.conn is None:
-            db_msg = "mysql connection error"
+        if not db_conn.is_connected():
+            msg = "mysql connection error"
         else:
-            db_msg = db_conn.insert_rows(data)
+            if db_conn.insert_from_csv(csv_file) == 1:
+                msg = f"successfully inserted csv file {csv_file.filename} data into db"
+            else:
+                msg = "can't insert csv to db"
 
+        db_conn.close_conn()
         res = make_response(
             jsonify(
-                {"message": db_msg}
+                {"message": msg}
             ),
         )
 
         return res
 
 
+@app.route('/upload_to_table', methods=['POST', 'GET'])
+def upload_csv():
+    if request.method == 'POST':
+        file = request.files['file']
+
+        csv_data = csv_to_list(file)
+
+        if csv_data == -1:
+            msg = 'not able to read csv file'
+        else:
+            db_conn = SqlDB(USERNAME, PASSWORD, HOST, DATABASE)
+
+            if db_conn._conn is None:
+                msg = "mysql connection error"
+            else:
+                msg = db_conn.insert_rows(csv_data)
+
+        res = make_response(
+            jsonify(
+                {"message": msg}
+            ),
+        )
+
+        return res
+
+    elif request.method == 'GET':
+        return render_template('index.html')
+
+
+@app.route('/insert/<string:table_name>', methods=['POST'])
+def insert_rows(table_name):
+    """
+    insert_rows(table_name), add rows to existing table.
+    """
+    json_data = request.get_json()
+    rows_data = json_data.get('rows')
+    rows = [list(row.values()) for row in rows_data]
+
+    db_conn = SqlDB(USERNAME, PASSWORD, HOST, DATABASE)
+    if not db_conn.is_connected():
+        msg = 'mysql server not connected'
+        logging.error(msg)
+        return make_response(msg)
+
+    if db_conn.check_table_exist(table_name):
+        try:
+            db_conn.insert_into_table(table_name,rows)
+            logging.debug(f'rows added to {table_name} table')
+            res = f'successfully inserted rows to {table_name} table'
+        except mySql_connector.errors.OperationalError as err:
+            logging.error(f"can't insert rows to {table_name} table, Error: {repr(err)}")
+            res = jsonify({'message':f"can't insert rows to {table_name} table"})
+    else:
+        res = jsonify({'message':f"{table_name} table not exist"})
+        logging.error(f"{table_name} table not exist")
+
+    db_conn.close_conn()
+    return make_response(res)
+
+
+@app.route('/read/<string:table_name>', methods=['GET'])
+def read_rows(table_name):
+    db_conn = SqlDB(USERNAME, PASSWORD, HOST, DATABASE)
+    if not db_conn.is_connected():
+        msg = 'mysql server not connected'
+        logging.error(msg)
+        return make_response(msg)
+
+    rows_data = db_conn.read_rows(table_name)
+
+    if rows_data != -1:
+        res = jsonify(rows_data)
+    else:
+        res = f'something wrong,can not read from {table_name} table'
+
+    db_conn.close_conn()
+
+    return make_response(res)
+
+
+@app.route('/delete/<string:table_name>/<string:item_id>', methods=['DELETE'])
+def delete_rows(table_name, item_id):
+    db_conn = SqlDB(USERNAME, PASSWORD, HOST, DATABASE)
+    if not db_conn.is_connected():
+        msg = 'mysql server not connected'
+        logging.error(msg)
+        return make_response(msg)
+
+    if db_conn.delete_row(table_name,item_id) != -1:
+        res = f'row deleted from {table_name} table'
+    else:
+        res = f'row did not deleted from {table_name} table'
+
+    return make_response(jsonify({"msg":res}))
+
+
+@app.route('/update/<string:table_name>/<string:item_id>', methods=['PUT'])
+def update_rows(table_name,item_id):
+    """
+    update_rows() func, update single record in a table.
+    """
+    json_data = request.get_json()
+
+    # make connection to database
+    db_conn = SqlDB(USERNAME, PASSWORD, HOST, DATABASE)
+    if not db_conn.is_connected():
+        msg = 'mysql server not connected'
+        logging.error(msg)
+        return make_response(msg)
+
+    if db_conn.update_row(table_name,item_id,json_data) == 1:
+        res = f'successfully updated record in {table_name} table'
+        logging.info(res)
+    else:
+        res = f'update get failed in {table_name} table'
+        logging.error(res)
+
+    return make_response(jsonify({"msg": res}))
+
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- Week_3/sql_db.py
+++ Week_3/sql_db.py
@@ -1,45 +1,197 @@
 import logging
+import time
 
 import mysql.connector as mySql_connector
 from mysql.connector import errorcode
 
+from Week_3.helpers import csv_to_list
+
 
 class SqlDB:
-    def __init__(self,username,password,host,database):
-        self.conn = None
+    """
+    SqlDB class is responsible for creating connection,manipulating and updating data on MySql database.
+    """
+
+    def __init__(self, username, password, host, database):
+        self._conn = None
         """
         connect to mysql server and save the connection object.
         """
         try:
-            self.conn = mySql_connector.connect(user=username, password=password, host=host, database=database)
+            self._conn = mySql_connector.connect(user=username, password=password, host=host, database=database)
             logging.info('connected to mysql server')
         except mySql_connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
                 logging.error(f"wrong username or password, Error: {repr(err)}")
             else:
-                logging.error(f"Something is wrong, Error: {repr(err)}")
+                logging.error(f"can't connect to MySql, Error: {repr(err)}")
 
-    def insert_rows(self,rows):
+    def insert_rows(self, rows):
         """
         insert list of rows into crypto_data table.
         """
-        cur = self.conn.cursor()
+        cur = self._conn.cursor()
         sql = "INSERT INTO crypto_data (id,symbol,name,image,current_price,market_cap,market_cap_rank,fully_diluted_valuation,total_volume,high_24h,low_24h,price_change_24h,price_change_percentage_24h,market_cap_change_24h,market_cap_change_percentage_24h,circulating_supply,total_supply,max_supply,ath,ath_change_percentage,ath_date,atl,atl_change_percentage,atl_date,roi,last_updated) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
         for row in rows:
             try:
                 cur.execute(sql, row)
             except mySql_connector.Error as err:
-                logging.error(f'sql query not executed, Error: {repr(err)}')
-                return "csv file can't upload."
+                logging.error(f'sql query get failed {sql}, Error: {repr(err)}')
+                return -1
 
-        self.conn.commit()
-        self.conn.close()
+        self._conn.commit()
+        self._conn.close()
 
         logging.debug('inserted all rows to crypto_data table')
 
-        return "csv file uploaded"
+        return 1
 
+    def insert_from_csv(self, csv_file):
+        csv_data = csv_to_list(csv_file)
 
+        if csv_data == -1:
+            return -1
+        else:
+            table_name = csv_file.filename.replace('.csv', '')
+            columns = csv_data[0]
+            rows = csv_data[1:]
 
+            # Check id column in csv file column list
+            if 'id' not in columns:
+                logging.error(f'{csv_file} file does not contain "id" column')
+                return -1
 
+            if self.check_table_exist(table_name):
+                cur_date = time.strftime('%Y_%m_%d_%H_%S')
+                table_name = "%s_%s" % (table_name, str(cur_date))
+                print(table_name)
+
+            if self.create_table(table_name, columns) == 1:
+                try:
+                    self.insert_into_table(table_name, rows)
+                    logging.debug(f'rows inserted into {table_name} table')
+                except mySql_connector.errors.OperationalError as err:
+                    logging.error(f"can't insert into {table_name} table, Error: {err}")
+                    return -1
+            else:
+                return -1
+        return 1
+
+    def check_table_exist(self, table_name):
+        query = f"SHOW TABLES LIKE '%s'" % table_name
+        db_cur = self._conn.cursor()
+        db_cur.execute(query)
+        result = db_cur.fetchone()
+
+        if result is None:
+            return False
+        else:
+            return True
+
+    def create_table(self, table_name, columns):
+        query = f"CREATE TABLE {table_name} ( id varchar(100) PRIMARY KEY," + " varchar(100),".join(columns) + " varchar(100) )"
+        cur = self._conn.cursor()
+        try:
+            cur.execute(query)
+            logging.debug(f'{table_name} table created successfully')
+            return 1
+        except mySql_connector.Error as err:
+            logging.error(f'sql query get failed {query}, Error: {repr(err)}')
+            return -1
+
+    def insert_into_table(self, table_name, rows):
+        value_places = ["%s" for i in range(len(rows[0]))]
+        query = f"INSERT INTO {table_name} VALUES (" + ",".join(value_places) + ")"
+
+        cur = self._conn.cursor()
+
+        try:
+            cur.executemany(query, rows)
+            self._conn.commit()
+            logging.debug(f'inserted rows into {table_name}')
+        except mySql_connector.Error as err:
+            logging.error(f'sql query get failed {query}, Error: {repr(err)}')
+            raise mySql_connector.errors.OperationalError(msg=f"sql query failed {query}, Error: {err}")
+
+    def read_rows(self, table_name):
+        query = f"SELECT * FROM {table_name}"
+
+        cur = self._conn.cursor()
+
+        try:
+            cur.execute(query)
+            row_headers = [x[0] for x in cur.description]
+            row_data = cur.fetchall()
+            json_data = []
+            for result in row_data:
+                json_data.append(dict(zip(row_headers, result)))
+            return json_data
+        except mySql_connector.Error as err:
+            logging.error(f'query failed {query}, Error: {repr(err)}')
+            return -1
+
+    def delete_row(self, table_name, item_id):
+        """
+        delete_row(), delete the rows from given table.
+        """
+        condition = "id='%s'" % item_id
+        query = f"DELETE FROM {table_name} WHERE {condition};"
+
+        cur = self._conn.cursor()
+
+        try:
+            cur.execute(query)
+            row_count = cur.rowcount
+            if row_count > 0:
+                logging.info(f'row deleted from {table_name} table')
+                self._conn.commit()
+                return 1
+            else:
+                logging.error(f'row did not deleted from {table_name}')
+                return -1
+        except mySql_connector.Error as err:
+            logging.error(f'query failed {query}, Error: {repr(err)}')
+            return -1
+
+    def update_row(self, table_name, item_id, update_values):
+        """
+        update() func, update record in a table.
+        """
+
+        # Prepare sql query for update record
+        new_values = ""
+        for key, value in update_values.items():
+            new_values += f"{key}='{value}',"
+        new_values = new_values[:-1]
+
+        query = f"UPDATE {table_name} SET {new_values} WHERE id='{item_id}'"
+
+        cur = self._conn.cursor()
+
+        try:
+            cur.execute(query)
+            if cur.rowcount > 0:
+                logging.debug(f'record updated in {table_name} table')
+                self._conn.commit()
+                return 1
+            else:
+                logging.error(f'query get failed {query}')
+        except mySql_connector.Error as err:
+            logging.error(f'can not execute query {query}, Error: {repr(err)}')
+            return -1
+
+    def is_connected(self):
+        """
+        return True if self._conn have mysql connection object else False'
+        """
+        if self._conn is None:
+            return False
+        else:
+            return True
+
+    def close_conn(self):
+        """
+        close the Database connection from mysql server.
+        """
+        self._conn.close()
